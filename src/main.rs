1906157b56b60c11822bc5d596afff57bcec21e1
Prabesh <prabesh91@hotmail.com>
2021-07-11 20:19:21 +0200

    added floating point numbers

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,21 @@ fn main() {
     
     println!("x is {}",x);
     x = 20; // unmuted variable
-
     println!("x is {}",x); // both lines are printed due to mutable variable
+
+    /* naming conventions 
+        rust-lang.github.io/api-guidelines/naming.html
+    */
+
+    let y: i32 = -10;
+    println!("y is {}",y);
+
+    // floating point numbers
+
+    let z:f32 = 3.1232939484934839;
+    println!("z is {}",z);
+    
+
 }
 
 
