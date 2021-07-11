78026d01fcac3bb411c61b3dd3741577026ec234
Prabesh <prabesh91@hotmail.com>
2021-07-11 19:58:03 +0200

    added comment on mutable variables

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let mut x = 10;
+    let mut x = 10; // mutable variable
     
     println!("x is {}",x);
-    x = 20;
+    x = 20; // unmuted variable
 
-    println!("x is {}",x);
+    println!("x is {}",x); // both lines are printed due to mutable variable
 }
 
 
