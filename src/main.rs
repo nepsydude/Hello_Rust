afc7a1191196279d4724e502847b2589e6dd56ac
nepsydude <prabesh91@hotmail.com>
2021-07-13 00:25:13 +0200

    functions in rust

--- src/main.rs
+++ src/main.rs
@@ -115,11 +115,41 @@ fn main() {
     println!("b is {}", b);
 
 
-
+    // functions
     
+    say_hello();
+    say_hello();
+    let x = 1;
+    let y = 2;
+    say_the_sum(x, y);
+    say_a_number(x as i32);
+
+    let result = square(13);
+    println!("result is {:?}", result);
 
 }   
 
+fn say_hello() {
+    println!("Hello!");
+    say_a_number(13);
+}
+
+// parameters in paranthesis
+fn say_a_number(number: i32) {
+    println!("number is {}", number);
+}
+
+
+fn say_the_sum(a: u8, b: u8) {
+    let sum = a + b;
+    println!("sum is {}", sum);
+}
+
+fn square(x: i32) -> (i32, i32) {
+    println!("squaring {}", x);
+    return (x, x * x);
+    println!("End of function");
+}
 
 
 
