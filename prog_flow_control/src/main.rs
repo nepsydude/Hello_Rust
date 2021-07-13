6d4be6f8d7b4e11bccd6c8ea837ffe05901ba15a
nepsydude <prabesh91@hotmail.com>
2021-07-13 09:57:36 +0200

    Loop examples, if , for , while, nested

--- prog_flow_control/src/main.rs
+++ prog_flow_control/src/main.rs
@@ -8,9 +8,9 @@ fn main() {
     }
 
     // Multiple conditions examples
-    
-    let x = 3;
-    let y = 5;
+
+    let x = 7;
+    let y = 7;
 
     if x > y {
         println!("x is greater than y");
@@ -29,4 +29,54 @@ fn main() {
     } else {
         println!("x is equal to y");
     }
+
+
+    // Loop examples
+
+    let mut count = 0;
+
+    let result = loop {
+        if count == 10 {
+            break count * 10;
+        }
+        count += 1;
+        println!("count is {}", count);
+    };
+
+    println!("After the loop!");
+    println!("result is {}", result);
+    let mut count = 0;
+    let letters = ['a', 'b', 'c'];
+
+    while count < letters.len() {
+        println!("letter is {}", letters[count]);
+        count += 1;
+    }
+    let message = ['h', 'e', 'l', 'l', 'o'];
+
+    for (index, &item) in message.iter().enumerate() {
+        println!("item {} is {}", index, item);
+        if item == 'e' {
+            break;
+        }
+    }
+
+    for number in 10..5 {
+        println!("number is {}", number);
+    }
+
+    // Nested loop example
+
+    let mut matrix = [[1, 2, 3],
+                      [4 ,5 ,6],
+                      [7, 8, 9]];
+
+    for row in matrix.iter_mut() {
+        for num in row.iter_mut() {
+           *num += 10;
+           print!("{}\t", num);
+        }
+        println!();
+    }
 }
+
